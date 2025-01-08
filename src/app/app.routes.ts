ff6be7af9888b0dbeb179247af50d8e437850d1d
Mohamed Ali <Mohamed.ali.awad2018@gmail.com>
2025-01-08 04:13:07 +0200

    add page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    {path:"",component:HomeComponent},
+{path:"",redirectTo:"Home",pathMatch:'full'},
 {path:"Home",component: HomeComponent},
 {path:"Contact", component:ContactComponent},
 {path:"Portfolio", component:PortfolioComponent},
-{path:"About",component:AboutComponent}
+{path:"About",component:AboutComponent},
+{path:"**",component:PageNotFoundComponent}
+
 
 ];
