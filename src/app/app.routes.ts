4f99ca191b58e0300579735fd88af54c0ab9836c
Mohamed Ali <Mohamed.ali.awad2018@gmail.com>
2025-01-08 03:56:41 +0200

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:HomeComponent},
+{path:"Home",component: HomeComponent},
+{path:"Contact", component:ContactComponent},
+{path:"Portfolio", component:PortfolioComponent},
+{path:"About",component:AboutComponent}
+
+];
